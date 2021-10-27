cdaad6d999ca63e0cb30b860c870a0a3f4654c5b
Ligia D'Avila Bozzi <ligia@respondeai.com.br>
2021-10-27 11:49:51 -0300

    preparando review

--- src/database/scripts/003-cria-compras.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE "purchases" (
-  "id" SERIAL PRIMARY KEY,
-  "customer_id" INTEGER NOT NULL,
-  "product_id" INTEGER NOT NULL,
-  CONSTRAINT "fk_purchases_customers" FOREIGN KEY("customer_id") REFERENCES "customers"("id"),
-  CONSTRAINT "fk_purchases_products" FOREIGN KEY("product_id") REFERENCES "products"("id")
-);
--- src/validation/wallet.js
+++
@@ -1,8 +0,0 @@
-import joi from 'joi';
-
-const validateRegister = joi.object({
-    description: joi.string().min(3).required(),
-    value: joi.number().required()
-});
-
-export { validateRegister };
